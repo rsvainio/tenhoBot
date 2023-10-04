2e2d54685d0bef5478cc74d5054aeff1cb1f18a8
rsvainio <rasvai@utu.fi>
2023-10-04 23:50:22 +0300

    Added skeleton for adding commands, and a shutdown command that checks for users with access

--- bot.py
+++ bot.py
@@ -1,28 +1,32 @@
 import discord
+from discord import Interaction
+from discord.ext import commands
 import json
 import databaseManipulation
 
+#TODO: Implement logging
 
-def loadConfig():
+def loadConfig(field):
     with open('config.json') as f:
-        data = json.load(f)
-        return data['token']
+        configs = json.load(f)
+        return configs[field]
     
 def runDiscordBot():
-    configs = loadConfig()
-    TOKEN = configs['token']
+    TOKEN = loadConfig('token')
     intents = discord.Intents.all()
-    client = discord.Client(intents=intents)
+    client = commands.Bot(command_prefix = '!', intents=intents)
 
     @client.event
     async def on_ready():
+        synced = await client.tree.sync()
+        print(f'Synced {len(synced)} commands')
         print(f'{client.user} is now running!')
 
     @client.event
     async def on_message(message):
         if message.author == client.user:
             return
-
+        
         username =      str(message.author)
         userMessage =   str(message.content)
         channel =       str(message.channel)
@@ -42,5 +46,20 @@ def runDiscordBot():
                 print('non-custom')
         except Exception as e:
             print(e)
+
+    @client.tree.command(name='hello')
+    async def hello(interaction: discord.Interaction):
+        await interaction.response.send_message('paska ei toimi')
+
+    @client.tree.command(name='tapan_ittes')
+    async def tapan_ittes(interaction: discord.Interaction):
+        print(f'{interaction.user} used: "{interaction.command}" on #{interaction.channel}')
+        if interaction.user.name == loadConfig('master') or interaction.user.name in loadConfig('whitelist'):
+            print('Shutting down...')
+            await interaction.response.send_message('kuolen nyt')
+            await client.close()
+        else:
+            #TODO: Do something fun here (permaban their ass)
+            await interaction.response.send_message('kuolet nyt')
 
     client.run(TOKEN)
--- databaseManipulation.py
+++ databaseManipulation.py
@@ -5,27 +5,40 @@ mutex = Lock()
 dbConn = None
 database = 'repostimies.db'
 
-def dbConnectDatabase(database):
-    conn = sqlite3.dbConnect(f'databases/{database}')
-    print('dbConnected to database:', database)
+def connectDatabase(database):
+    conn = sqlite3.connect(f'databases/{database}')
+    print('connected to database:', database)
+    global dbConn
     dbConn = conn
 
-def updateDatabase(value1):
+#TODO make this more abstract, (i.e. accept a list of values and parse the sql syntax to understand what the command is)
+def queryDatabase(value1):
     if dbConn == None:
-        dbConnectDatabase(database)
+        connectDatabase(database)
     with mutex:
-        print('Starting query')
         cursor = dbConn.cursor()
 
-        #TODO fix this shit
-        sql = f'UPDATE Repostimies SET Num_reacted = 2 WHERE Username = ?'
+        #TODO should make this more dynamic
+        sql = 'SELECT Username FROM Repostimies WHERE Username = ?'
         cursor.execute(sql, [(value1)])
 
-        dbConn.commit()
+        if cursor.fetchone():
+            print(f'Incrementing the Num_reacted value of user: {value1}')
+            sql = 'UPDATE Repostimies SET Num_reacted = Num_reacted + 1 WHERE Username = ?'
+        else:
+            print(f'User {value1} not found within the database, inserting...')
+            sql = 'INSERT INTO Repostimies VALUES (?, 1)'
+
+        try:
+            cursor.execute(sql, [(value1)])
+            dbConn.commit()
+        #TODO add logging here
+        except sqlite3.ProgrammingError() as e:
+            print(e)
 
 def databaseReset(database):
     if dbConn == None:
-        dbConnectDatabase(database)
+        connectDatabase(database)
 
     cursor = dbConn.cursor()
     cursor.execute('DROP TABLE IF EXISTS Repostimies')
