f94a74753b3d9c3f3f20f62d4d4f6f1f1b8c11a2
rsvainio <rasvai@utu.fi>
2023-10-05 01:12:52 +0300

    Added examples for config and responses, and added a function for adding new responses

--- bot.py
+++ bot.py
@@ -6,11 +6,6 @@ import databaseManipulation
 
 #TODO: Implement logging
 
-def loadConfig(field):
-    with open('config.json') as f:
-        configs = json.load(f)
-        return configs[field]
-    
 def runDiscordBot():
     TOKEN = loadConfig('token')
     intents = discord.Intents.all()
@@ -18,15 +13,12 @@ def runDiscordBot():
 
     @client.event
     async def on_ready():
-        synced = await client.tree.sync()
-        print(f'Synced {len(synced)} commands')
         print(f'{client.user} is now running!')
 
     @client.event
     async def on_message(message):
         if message.author == client.user:
             return
-        
         username =      str(message.author)
         userMessage =   str(message.content)
         channel =       str(message.channel)
@@ -49,12 +41,33 @@ def runDiscordBot():
 
     @client.tree.command(name='hello')
     async def hello(interaction: discord.Interaction):
-        await interaction.response.send_message('paska ei toimi')
+        await interaction.response.send_message('stfu')
+
+    @client.tree.command(name='add_response')
+    async def add_response(interaction: discord.Interaction, response: str):
+        try:
+            response = str(response)
+            if response == None:
+                raise Exception
+        except Exception as e:
+            print(e)
+            await interaction.response.send_message('Use a proper response dumbass')
+            return
+
+        with open('responses.json') as f:
+            responses = json.load(f)
+        responses.append({
+            "author": interaction.user.name,
+            "response": response})
+        with open('responses.json', 'w') as f:
+            json.dump(responses, f)
+
+        await interaction.response.send_message('Thanks for your input, sucker\nThis is going in my cringe compilation')
 
     @client.tree.command(name='tapan_ittes')
     async def tapan_ittes(interaction: discord.Interaction):
         print(f'{interaction.user} used: "{interaction.command}" on #{interaction.channel}')
-        if interaction.user.name == loadConfig('master') or interaction.user.name in loadConfig('whitelist'):
+        if await client.is_owner(interaction.user) or interaction.user.name in loadConfig('whitelist'):
             print('Shutting down...')
             await interaction.response.send_message('kuolen nyt')
             await client.close()
@@ -62,4 +75,17 @@ def runDiscordBot():
             #TODO: Do something fun here (permaban their ass)
             await interaction.response.send_message('kuolet nyt')
 
+    @client.tree.command(name='sync')
+    @commands.guild_only()
+    @commands.is_owner()
+    async def sync(interaction: discord.Interaction):
+        synced = await client.tree.sync()
+        print(f'Synced {len(synced)} commands')
+        await interaction.response.send_message(f'Synced {len(synced)} commands')
+
     client.run(TOKEN)
+
+def loadConfig(field):
+    with open('config.json') as f:
+        configs = json.load(f)
+        return configs[field]
