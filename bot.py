09354e45ec8921b6c7f24f54f5524e067378b3eb
rsvainio <rasvai@utu.fi>
2023-10-05 02:29:18 +0300

    Changed from a message-based response to embeds

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import discord
 from discord import Interaction
 from discord.ext import commands
 import json
+import random
 import databaseManipulation
 
 #TODO: Implement logging
@@ -9,15 +10,15 @@ import databaseManipulation
 def runDiscordBot():
     TOKEN = loadConfig('token')
     intents = discord.Intents.all()
-    client = commands.Bot(command_prefix = '!', intents=intents)
+    bot = commands.Bot(command_prefix = '!', intents=intents)
 
-    @client.event
+    @bot.event
     async def on_ready():
-        print(f'{client.user} is now running!')
+        print(f'{bot.user} is now running!')
 
-    @client.event
-    async def on_message(message):
-        if message.author == client.user:
+    @bot.event
+    async def on_message(message: discord.Message):
+        if message.author == bot.user:
             return
         username =      str(message.author)
         userMessage =   str(message.content)
@@ -27,7 +28,7 @@ def runDiscordBot():
         if userMessage.lower().startswith('hello'):
             await message.channel.send('YO!')
 
-    @client.event
+    @bot.event
     # catch 'repostimiespate'
     async def on_reaction_add(reaction: discord.Reaction, user: discord.Member):
         try:
@@ -39,11 +40,17 @@ def runDiscordBot():
         except Exception as e:
             print(e)
 
-    @client.tree.command(name='hello')
+    @bot.tree.command(name='hello', description='Greet the bot and get a "witty" response')
     async def hello(interaction: discord.Interaction):
-        await interaction.response.send_message('stfu')
+        with open('responses.json') as f:
+            responses = json.load(f)
+            response = random.choice(responses)
+        embed = embedDecorator(interaction)
+        embed.add_field(name=response['response'], value='')
+        embed.set_footer(text=f"Author: {response['author']}")
+        await interaction.response.send_message(embed=embed)
 
-    @client.tree.command(name='add_response')
+    @bot.tree.command(name='add_response', description="Add a command to the bot's list of responses used in the /hello command")
     async def add_response(interaction: discord.Interaction, response: str):
         try:
             response = str(response)
@@ -51,7 +58,9 @@ def runDiscordBot():
                 raise Exception
         except Exception as e:
             print(e)
-            await interaction.response.send_message('Use a proper response dumbass')
+            embed = embedDecorator(interaction)
+            embed.add_field(name='Use a proper response, dumbass', value='')
+            await interaction.response.send_message(embed=embed)
             return
 
         with open('responses.json') as f:
@@ -62,30 +71,44 @@ def runDiscordBot():
         with open('responses.json', 'w') as f:
             json.dump(responses, f)
 
-        await interaction.response.send_message('Thanks for your input, sucker\nThis is going in my cringe compilation')
+        embed = embedDecorator(interaction)
+        embed.add_field(name="Thanks for your input, sucker\nThis is going straight in my cringe collection", value='')
+        await interaction.response.send_message(embed=embed)
 
-    @client.tree.command(name='tapan_ittes')
+    @bot.tree.command(name='tapan_ittes', description="Make the bot sad and tell it to shut down")
     async def tapan_ittes(interaction: discord.Interaction):
         print(f'{interaction.user} used: "{interaction.command}" on #{interaction.channel}')
-        if await client.is_owner(interaction.user) or interaction.user.name in loadConfig('whitelist'):
-            print('Shutting down...')
-            await interaction.response.send_message('kuolen nyt')
-            await client.close()
-        else:
-            #TODO: Do something fun here (permaban their ass)
-            await interaction.response.send_message('kuolet nyt')
+        embed = embedDecorator(interaction)
 
-    @client.tree.command(name='sync')
+        if await bot.is_owner(interaction.user) or interaction.user.name in loadConfig('whitelist'):
+            print('Shutting down...')
+            embed.add_field(name='kuolen nyt', value='')
+            await interaction.response.send_message(embed=embed)
+            await bot.close()
+        else:
+            #TODO: Do something fun here (read: permaban their ass)
+            embed.add_field(name='kuolet nyt', value='')
+            await interaction.response.send_message(embed=embed)
+
+    @bot.tree.command(name='sync', description="Update the bot's list of commands")
     @commands.guild_only()
     @commands.is_owner()
     async def sync(interaction: discord.Interaction):
-        synced = await client.tree.sync()
+        synced = await bot.tree.sync()
         print(f'Synced {len(synced)} commands')
-        await interaction.response.send_message(f'Synced {len(synced)} commands')
+        embed = embedDecorator(interaction)
+        embed.add_field(name=f'Synced {len(synced)} commands', value='')
+        await interaction.response.send_message(embed=embed, ephemeral=True)
 
-    client.run(TOKEN)
+    bot.run(TOKEN)
 
 def loadConfig(field):
     with open('config.json') as f:
         configs = json.load(f)
         return configs[field]
+
+# general decorator for embeds that can be expanded upon later
+def embedDecorator(context: discord.Interaction) -> discord.Embed:
+    embed = discord.Embed()
+    embed.set_author(name=context.user.display_name, icon_url=context.user.display_avatar.url)
+    return embed
