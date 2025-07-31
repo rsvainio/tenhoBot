83ee2cff321d6ffc059cf7ab21ebc6f0c6c48a26
rsvainio <rasmus.vainio@gmail.com>
2025-07-31 13:42:52 +0300

    Changed the music player implementation into a cog from an extension

--- musicPlayer.py
+++ musicPlayer.py
@@ -1,50 +1,65 @@
-import discord
+from discord import app_commands
 from discord.ext import commands
 from pytubefix import YouTube
 from pytubefix import Search
 from pytubefix.cli import on_progress
+import json
 
-#TODO: should make a list of downloaded videos so that they don't need to be redownloaded
-downloadFolder: str = 'media/videos'
+class MusicPlayer(commands.Cog):
+    def __init__(self, bot):
+        self.bot: commands.Bot = bot
+        self.downloadFolder: str = 'media/videos'
+        self.videoMetadata: dict = self.loadVideoMetadata()
 
-@commands.command()
-async def addUrl(url: str):
-    await downloadVideo(url)
-    playVideo()
-    #player = discord.FFmpegPCMAudio(source, executable='utils/FFmpeg/bin/ffmpeg.exe')
+    def addUrl(self, url: str):
+        self.downloadVideo(url)
+        self.playVideo()
+        print("asd")
+        #player = discord.FFmpegPCMAudio(source, executable='utils/FFmpeg/bin/ffmpeg.exe')
 
-async def addQuery(query: str):
-    results = Search(query)
-    await downloadVideo(results.videos[0].watch_url) # download the first video in search results
-    playVideo()
+    def addQuery(self, query: str):
+        results = Search(query)
+        self.downloadVideo(results.videos[0].watch_url) # download the first video in search results
+        self.playVideo()
 
-async def playVideo():
-    return
+    def playVideo(self):
+        return
 
-async def downloadVideo(url: str):
-    filename = isInVideoCache(url)
+    def downloadVideo(self, url: str):
+        filename = self.isInVideoCache(url)
 
-    if not filename:
-        yt = YouTube(url, on_progress_callback=on_progress)
-        print(yt.title)
+        if not filename:
+            yt = YouTube(url, on_progress_callback=on_progress)
+            print(yt.title)
 
-        ys = yt.streams.get_audio_only()
-        filename = ys.download(output_path=downloadFolder)
-        filename = shortenFilepath(filename)
-        # add the newly downloaded file to the metadata file here
+            ys = yt.streams.get_audio_only()
+            filename = ys.download(output_path=self.downloadFolder)
+            filename = self.shortenFilepath(filename)
+            # add the newly downloaded file to the metadata file here
 
-    return filename
+        return filename
 
+    def isInVideoCache(self, url) -> str:
+        # search through a metadata file and return the parameter url's corresponding video file's 
+        # name if it exists and return it in f'media/videos/{filename}' format
+        return ''
 
-def isInVideoCache(url) -> str:
-    # search through a metadata file and return the parameter url's corresponding video file's 
-    # name if it exists and return it in f'media/videos/{filename}' format
-    return ''
+    @staticmethod
+    def shortenFilepath(path: str) -> str:
+        marker = 'media/videos/'
+        index = path.find(marker)
+        if index != -1:
+            return path[index:]
+        else:
+            return path  # return the original string if marker is not found
 
-def shortenFilepath(path: str) -> str:
-    marker = 'media/videos/'
-    index = path.find(marker)
-    if index != -1:
-        return path[index:]
-    else:
-        return path  # return the original string if marker is not found
+    @staticmethod
+    def loadVideoMetadata() -> dict:
+        with open('media/videoMetadata.json', encoding='utf-8') as f:
+            return
+
+if __name__ == '__main__':
+    MusicPlayer(commands.Bot).addUrl("https://www.youtube.com/watch?v=kofR7f7oNnE")
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(MusicPlayer(bot = bot))
